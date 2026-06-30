9ad5929c2132f814c907edba0e01c9f74bd1814a
Jeroen de Bruijn <vv9239@gmail.com>
2026-06-30 07:02:19 +0200

    chore(deps): update dependency @vidavidorra/eslint-config to v2 and eslint to v10

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,16 +2,16 @@ import test from 'ava';
 import {check, format} from 'prettier';
 import config from './index.js';
 
-test('exports an object as "default"', t => {
+test('exports an object as "default"', (t) => {
   t.is(typeof config, 'object');
 });
 
-test('"check" can use the config', async t => {
-  t.is(await check('const x = 1;\n', {...config, parser: 'typescript'}), true);
-  t.is(await check('const x = 1\n', {...config, parser: 'typescript'}), false);
+test('"check" can use the config', async (t) => {
+  t.true(await check('const x = 1;\n', {...config, parser: 'typescript'}));
+  t.false(await check('const x = 1\n', {...config, parser: 'typescript'}));
 });
 
-test('"format" can use the config', async t => {
+test('"format" can use the config', async (t) => {
   t.is(
     await format('const x = 1', {...config, parser: 'typescript'}),
     'const x = 1;\n',
